8f007db6f0a5a00db2e2bd6f54e1a968c36fd910
adabarbulescu <94562950+adabarbulescu@users.noreply.github.com>
2022-01-12 19:16:38 +0200

    Create hello.c

---
+++ Problem set 1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //get the input from the user
+    string name = get_string("What is your name?\n");
+    //print the welcoming message
+    printf("hello, %s\n", name);
+}
